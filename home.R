ba4877d9cb86a51911bb6e0ebb37a1168888a3eb
Obi Thompson Sargoni <33906525+obisargoni@users.noreply.github.com>
2019-11-21 11:40:02 +0000

    Geocoding the data and exporting the result

--- home.R
+++ home.R
@@ -1,7 +1,19 @@
 # R Script git intro
+library("tmaptools")
+library("jsonlite")
+setwd("C:\\Users\\Obi\\Desktop\\CASA\\Git-Intro")
 
 name <- c("obi")
 home_town <- c("london")
 scared_of <- c("dracula")
 
 df <- data.frame(name, home_town, scared_of)
+
+# Geocoding data
+df <- df[, c("name", "home_town", "scared_of")]
+dfGeo <- tmaptools::geocode_OSM(as.vector(df$home_town), as.data.frame = TRUE)
+df <- merge(df, dfGeo, by.x = "home_town", by.y = "query")
+print(df)
+
+df <- df[, c("lat", "lon", "name", "home_town", "scared_of")]
+write.table(df, "location01.csv", col.names = FALSE, row.names = FALSE, sep = ",")
