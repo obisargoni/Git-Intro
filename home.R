058cd61632f20a8c63785c75d82b4a650642dc1d
Obi Sargoni <othompsarg@hotmail.com>
2019-11-22 12:19:59 +0000

    Removed scared of field

--- home.R
+++ home.R
@@ -5,15 +5,14 @@ setwd("C:\\Users\\Obi\\Desktop\\CASA\\Git-Intro")
 
 name <- c("obi")
 home_town <- c("london")
-scared_of <- c("dracula")
 
-df <- data.frame(name, home_town, scared_of)
+df <- data.frame(name, home_town)
 
 # Geocoding data
-df <- df[, c("name", "home_town", "scared_of")]
+df <- df[, c("name", "home_town")]
 dfGeo <- tmaptools::geocode_OSM(as.vector(df$home_town), as.data.frame = TRUE)
 df <- merge(df, dfGeo, by.x = "home_town", by.y = "query")
 print(df)
 
-df <- df[, c("lat", "lon", "name", "home_town", "scared_of")]
+df <- df[, c("lat", "lon", "name", "home_town")]
 write.table(df, "location01.csv", col.names = FALSE, row.names = FALSE, sep = ",")
